0f2c91bf85f4f920c41c7ada341e3c7a79bf8080
Bill Lee <ood11611doo@gmail.com>
2025-12-30 09:22:12 +0900

    251230 - Updates Question 035

---
+++ Codes/260206.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = money;
+    for (int i = 1; i <= count; i++) {
+        answer -= price * i;
+    }
+    if (answer >= 0) answer = 0;
+    answer = abs(answer);
+    return answer;
+}
